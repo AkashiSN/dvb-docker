a8ca76b8e1ae315cdf2e2e07f497ebd009fa51fe
AkashiSN <btorntireinvynriy@gmail.com>
2021-05-05 19:29:28 +0900

    rename dir

--- v4l-utils/dvb-scan/scan.sh
+++ v4l-utils/dvb-scan/scan.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-dvbv5-scan -C JP -a 1 -o /workdir/tuners/isdbt.conf /workdir/isdb-initial/jp-ISDBT-ALL
-dvbv5-scan -C JP -a 0 -N -T 5 -o /workdir/tuners/isdbs-bs.conf /workdir/isdb-initial/jp-ISDBS-BS
-dvbv5-scan -C JP -a 0 -N -T 5 -o /workdir/tuners/isdbs-cs.conf /workdir/isdb-initial/jp-ISDBS-CS
+dvbv5-scan -C JP -a 1 -o /workdir/tuners/isdbt.conf /workdir/isdb-init/jp-ISDBT-ALL
+dvbv5-scan -C JP -a 0 -N -T 5 -o /workdir/tuners/isdbs-bs.conf /workdir/isdb-init/jp-ISDBS-BS
+dvbv5-scan -C JP -a 0 -N -T 5 -o /workdir/tuners/isdbs-cs.conf /workdir/isdb-init/jp-ISDBS-CS
 
 cat /workdir/tuners/isdbt.conf /workdir/tuners/isdbs-bs.conf /workdir/tuners/isdbs-cs.conf > /workdir/tuners/isdb-all.conf
