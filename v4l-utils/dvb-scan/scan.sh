a5a2ff3c422daabee5ea04e2b74dad3999fcccba
AkashiSN <btorntireinvynriy@gmail.com>
2021-05-06 13:03:36 +0900

    update channel scan

--- v4l-utils/dvb-scan/scan.sh
+++ v4l-utils/dvb-scan/scan.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -eu
 
-dvbv5-scan -C JP -a 1 -o /workdir/tuners/isdbt.conf /workdir/isdb-init/jp-ISDBT-ALL
-dvbv5-scan -C JP -a 0 -N -T 5 -o /workdir/tuners/isdbs-bs.conf /workdir/isdb-init/jp-ISDBS-BS
-dvbv5-scan -C JP -a 0 -N -T 5 -o /workdir/tuners/isdbs-cs.conf /workdir/isdb-init/jp-ISDBS-CS
+dvbv5-scan -C JP -a 0 -o /workdir/tuners/isdbt-uhf.conf /workdir/isdb-init/jp-ISDBT-UHF
+dvbv5-scan -C JP -a 1 -N -T 5 -o /workdir/tuners/isdbs-bs.conf /workdir/isdb-init/jp-ISDBS-BS
+dvbv5-scan -C JP -a 1 -N -T 5 -o /workdir/tuners/isdbs-cs.conf /workdir/isdb-init/jp-ISDBS-CS
 
-cat /workdir/tuners/isdbt.conf /workdir/tuners/isdbs-bs.conf /workdir/tuners/isdbs-cs.conf > /workdir/tuners/isdb-all.conf
+echo "# UHF" > /workdir/tuners/isdb.conf
+cat /workdir/tuners/isdbt-uhf.conf >> /workdir/tuners/isdb.conf
+echo "# BS" >> /workdir/tuners/isdb.conf
+cat /workdir/tuners/isdbs-bs.conf >> /workdir/tuners/isdb.conf
+echo "# CS" >> /workdir/tuners/isdb.conf
+cat /workdir/tuners/isdbs-cs.conf >> /workdir/tuners/isdb.conf
+
+echo "Saved channel conf in ./tuners/isdb.conf"
